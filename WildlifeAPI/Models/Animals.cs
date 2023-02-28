08c72df2144a74e01ea99a813290db4c3c867cc0
AmielSaballo <saballoamiel@gmail.com>
2023-02-28 23:30:00 +0800

    modified Animals model

--- WildlifeAPI/Models/Animals.cs
+++ WildlifeAPI/Models/Animals.cs
@@ -3,11 +3,11 @@
     public class Animals
     {
         public int id { get; set; }
-        public string Category { get; set; }
-        public string ScientificName { get; set; }
-        public string CommonName { get; set; }
-        public string Status { get; set; }
-        public string Location { get; set; }
+        public string? Category { get; set; }
+        public string? ScientificName { get; set; }
+        public string? CommonName { get; set; }
+        public string? Status { get; set; }
+        public string? Location { get; set; }
 
     }
 }
