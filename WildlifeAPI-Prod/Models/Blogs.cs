a8ac1d68b8fe965025429eb3740e7382be350865
AmielSaballo <saballoamiel@gmail.com>
2023-03-01 16:29:03 +0800

    modified blogs model

--- WildlifeAPI-Prod/Models/Blogs.cs
+++ WildlifeAPI-Prod/Models/Blogs.cs
@@ -4,9 +4,11 @@
     {
         public int id { get; set; }
         public string? blogTitle { get; set; }
+
+        public string? blogSummary { get; set; }
         public string? blogContent { get; set; }
-        public string? author { get; set; }
-        public string? datePosted { get; set; }
-        public string? image { get; set; }
+        public string? blogAuthor { get; set; }
+        public string? postedDate { get; set; }
+        public string? blogImage { get; set; }
     }
 }
